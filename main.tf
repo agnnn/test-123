ab6ecd1afb90ea96d202ee540a1c83373f620019
Pradeep Ravindran <11547169+agnnn@users.noreply.github.com>
2025-03-11 11:54:39 +0000

    test

--- main.tf
+++ main.tf
@@ -1,7 +1,9 @@
 provider "google" {
   project = "pradeep-sandbox"
   region  = "us-central1"
+  credentials = jsondecode(var.google_credentials)
 }
+variable "google_credentials" {}
 
 # 🚨 Intentionally insecure storage bucket (public access enabled) 🚨
 resource "google_storage_bucket" "public_bucket" {
